efbd9215680eeabedbd4bfd631e85be60898f4b3
mugeshkumar <kmugesh11199@gmail.com>
2022-10-02 17:44:06 +0530

    add new file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var startEle = document.getElementById("start");
+var calculateEle = document.getElementById("calculated");
 var problemsEle= document.getElementsByClassName("problems")[0];
 
 
@@ -36,19 +37,20 @@ problemEle.setAttribute('class', 'problem');
 
 //create h3 for number1
 let number1Ele=document.createElement('h3');
-
+number1Ele.setAttribute('class','number1');
 //update value of number1
 number1Ele.innerText = number1
 
 //create h3 for operation
 let operationEle = document.createElement('h3');
 operationEle.innerText = textTosymbol(operation);
-
+operationEle.setAttribute('class','operation');
 //update sympol of operation
 
 
 //create h3 for number2 
 let number2Ele=document.createElement('h3');
+number2Ele.setAttribute('class','number2');
 
 //update value of number2
 number2Ele.innerText = number2
@@ -66,10 +68,10 @@ inputEle.setAttribute('class','answer');
 inputEle.setAttribute('type','text');
 
 //append h3s,hr,input inside problems
-problemEle.append(number1)
-problemEle.append(operationEle)
-problemEle.append(number2Ele)
-problemEle.append(inputEle)
+problemEle.append(number1Ele);
+problemEle.append(operationEle);
+problemEle.append(number2Ele);
+problemEle.append(inputEle);
 
 //append inside problems  
 problemsEle.append(problemEle);
@@ -107,6 +109,44 @@ var stage1operation = function(){
 
 
 }
+function stage2operation(){
+    var problemElements = document.getElementsByClassName('problem'),
+    problemEle, operationEle, operationType, number1Ele, number1value, number2Ele, number2value, expectedAnswer, actualAnswerEle, actualAnswer, mark = 0;
+    
+    for(let i=0; i<problemElements.length; i++){
+        problemEle = problemElements[i];
+        //find operation
+         operationEle = problemEle.getElementsByClassName('operation')[0];
+         operationType = operationEle.innerText;
+        //get number 1 value
+          number1Ele = problemEle.getElementsByClassName('number1')[0];
+          number1value =parseInt(number1Ele.innerText);
 
+        //get number2 value
+         number2Ele = problemEle.getElementsByClassName('number2')[0];
+         number2value = parseInt(number2Ele.innerText);
+
+        //find expected answer
+        if(operationType == '+'){
+           expectedAnswer = number1value + number2value;
+        }else if(operationType == '-'){
+            expectedAnswer = number1value - number2value;
+        }else if(operationType == '*'){
+            expectedAnswer = number1value * number2value;
+        }else{
+            expectedAnswer = number1value/number2value;
+        }
+        // get actual answer
+            actualAnswerEle = problemEle.getElementsByClassName('answer')[0];
+            actualAnswer = parseInt (actualAnswerEle.value);
+        //compare both answers
+        if(expectedAnswer == actualAnswer){
+            mark++;
+        }
+        //if true and false
+    }
+    alert("your mark is"+mark);
+}
 
 startEle.addEventListener('click',stage1operation);
+calculateEle.addEventListener('click',stage2operation);
